2bfc590e5cc2441ce379dec6ed91580a032ddebc
Sean Isaiah Gulinao <seanisaiah.gulinao@learnet.se>
2018-01-15 12:17:22 +0100

    Comments about myself

--- BroadCaSeanGitHub/BroadCaSeanGitHub/Program.cs
+++ BroadCaSeanGitHub/BroadCaSeanGitHub/Program.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace BroadCaSeanGitHub
 {
+    //Created by Sean Isaiah Gulinao   2018-01-15
+    //From C-sharpskolan
+
     class Program
     {
         static void Main(string[] args)
